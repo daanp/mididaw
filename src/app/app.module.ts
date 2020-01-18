fdd592bcb5fa24c3866e4641334a5bd975c4e05d
Daan Paardekooper <daanp@Daans-MacBook-Pro.local>
2020-01-18 19:36:57 +0100

    add partials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MidiSelectorComponent } from './midi/midi-selector.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCardModule, MatFormFieldModule, MatSelectModule, MatSliderModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatSliderModule} from '@angular/material';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { SynthComponent } from './synth/synth.component';
 
@@ -23,7 +23,8 @@ import { SynthComponent } from './synth/synth.component';
     ReactiveFormsModule,
     MatCardModule,
     MatSliderModule,
-    FormsModule
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
